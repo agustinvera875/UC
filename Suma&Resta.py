e9fc3e2a7d2ee18d5e0d15589bd4a1f165a1d6a5
agustinvera875 <agus.vera@duocuc.cl>
2026-06-09 16:13:19 -0400

    primera carga de archivos
    se agrega funciones sumar y restar

---
+++ Suma&Resta.py
@@ -0,0 +1,11 @@
+def sumar(a, b):
+    return a + b
+
+def restar(a, b):
+    return a - b
+
+resultado_suma = sumar(10, 5)
+resultado_resta = restar(10, 5)
+
+print("Suma:", resultado_suma)    # 15
+print("Resta:", resultado_resta)  # 5
